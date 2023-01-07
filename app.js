2007a3ba16acbf952a2243cde9e1f7f6c7b3e673
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-01-07 11:54:32 -0500

    added script to change color of background on botton

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-window.onload = function () {
+window.onload = init;
+function init() {
     const body = document.querySelector('body')
-    const main  = document.querySelector('main')
-    const button = document.querySelector('button')
+    const main = document.querySelector('main')
+    const reset = document.querySelector('.reset')
     const hex = document.querySelector('.hex_val')
 
-    hex.innerHTML('hello')
+    reset.onclick = function () {
+        console.log('clickeddd')
+    };
+
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+
+    body.style.backgroundColor = getRandomColor();
+    hex.innerHTML = getRandomColor();
+
+    // Set the random color as the background-color of the div when the reset is clicked
+    reset.addEventListener("click", function () {
+        body.style.backgroundColor = getRandomColor();
+        hex.innerHTML = getRandomColor();
+    });
+
 
 }
