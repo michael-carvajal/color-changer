da2e8267cc13823461869743547914ba23e3fc88
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-01-07 15:38:45 -0500

    add finction to save color on screen

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ function init() {
     const main = document.querySelector('main')
     const reset = document.querySelector('.reset')
     const hex = document.querySelector('.hex_val')
-
+    const user_save = document.querySelector('.user_save')
+    const saved_display = document.querySelector('.saved_display')
     reset.onclick = function () {
         console.log('clickeddd')
     };
@@ -17,12 +18,38 @@ function init() {
     body.style.backgroundColor = randomColor;
     hex.innerHTML = randomColor;
 
+    let saved = [];
+    saved.push(randomColor)
     // Set the random color as the background-color of the div when the reset is clicked
     reset.addEventListener("click", function () {
         let randomColor = getRandomColor()
         body.style.backgroundColor = randomColor;
         hex.innerHTML = randomColor;
+        // Save random color
+        saved.push(randomColor);
     });
+
+    user_save.addEventListener("click", function () {
+        let saved_array = [];
+        saved_array.push(saved[saved.length - 1])
+        console.log(saved_array);
+
+        function addSquare() {
+            var newDiv = document.createElement("div");
+            newDiv.style.height = "100px";
+            newDiv.style.width = "100px";
+            newDiv.style.backgroundColor = saved_array[0];
+            newDiv.style.border = "2px solid black"
+            newDiv.style.margin = "10px"
+
+            // Append the new div to the container element
+            saved_display.appendChild(newDiv);
+        }
+        addSquare();
+
+    })
+
+
 
 
 }
