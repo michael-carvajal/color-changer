b756f3d6c40a1e9301d711da557a53d617d12605
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-01-07 11:29:03 -0500

    initializing

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    const body = document.querySelector('body')
+    const main  = document.querySelector('main')
+    const button = document.querySelector('button')
+    const hex = document.querySelector('.hex_val')
+
+    hex.innerHTML('hello')
+
+}
